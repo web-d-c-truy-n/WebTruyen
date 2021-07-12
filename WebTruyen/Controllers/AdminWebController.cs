c827f26b19b21253bb4e4402a0d75626c6c4f1bb
azdoan1604 <thaodoan.2x06@gmail.com>
2021-07-12 20:12:18 +0700

    updatelayoutadmin1

---
+++ WebTruyen/Controllers/AdminWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTruyen.Controllers
+{
+    public class AdminWebController : Controller
+    {
+        // GET: AdminWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
