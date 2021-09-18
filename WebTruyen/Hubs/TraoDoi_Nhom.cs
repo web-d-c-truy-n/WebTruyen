cd6156079db706db06b8e609a3e37b4f0e96b67f
anphuc99 <86525411+anphuc99@users.noreply.github.com>
2021-09-18 14:46:46 +0700

    Xong

--- WebTruyen/Hubs/TraoDoi_Nhom.cs
+++ WebTruyen/Hubs/TraoDoi_Nhom.cs
@@ -26,15 +26,17 @@ namespace WebTruyen.Hubs
             return Groups.Remove(Context.ConnectionId, roomName);
         }
 
-        public void Message(int maNhom, string noiDung)
+        public void Message(int maTV,int maNhom, string noiDung)
         {
+            webtruyenptEntities db = new webtruyenptEntities();
             string groupName = maNhom+"";
             TraoDoiNhom traoDoiNhom = new TraoDoiNhom();
             traoDoiNhom.MaNhom = maNhom;
-            traoDoiNhom.MaTV = Auth.MaTk();
+            traoDoiNhom.MaTV = maTV;
             traoDoiNhom.NoiDung = noiDung;
             HanhDongCuaTK hanhDongCuaTK = new HanhDongCuaTK();
-            //hanhDongCuaTK.traoDoiNhom(traoDoiNhom);
+            hanhDongCuaTK.traoDoiNhom(traoDoiNhom);
+            traoDoiNhom = db.TraoDoiNhoms.FirstOrDefault(x => x.MaTraoDoi == hanhDongCuaTK.MaHD);
             Clients.Group(groupName).message(new { traoDoiNhom.MaNhom, traoDoiNhom.MaTV, traoDoiNhom.MaTraoDoi, traoDoiNhom.NoiDung, traoDoiNhom.PhanHoi, NgayViet = traoDoiNhom.NgayViet.ToString("dd/MM/yyyy"), traoDoiNhom.ButDanh, traoDoiNhom.Avatar });
         }
     }
--- WebTruyen/Scripts/react/reactTraoDoi.js
+++ WebTruyen/Scripts/react/reactTraoDoi.js
@@ -1,17 +1,34 @@
 ﻿var dataChat = []
 var chat
 class TraoDoi extends React.Component {
-    chat = (e) => {
-        let noiDung = $(e).val()
-        chat.server.message(maNhom, noiDung)
+    chat = () => {        
+        let noiDung = $("#message").val()
+        $.connection.hub.start().done(function () {
+            chat.server.message(maTV, maNhom, noiDung)
+        })
+        $("#message").val("")
+    }
+    enterChat = (e) => {        
+        if (e.keyCode == 13) {
+            this.chat()
+        }
+    }
+    componentDidMount() {
+        this.componentDidUpdate()
+    }
+
+    componentDidUpdate() {
+        debugger
+        let top = $("#phanChat")
+        $("#phanChat").scrollTop(10000000000)
     }
     render() {        
         let data = this.props.data
         let toiChat = (x) => (
             <div class="direct-chat-msg right">
                 <div class="direct-chat-infos clearfix">
-                    <span class="direct-chat-name float-right">{x.ButDanh}</span>
-                    <span class="direct-chat-timestamp float-left">{x.NgayViet}</span>
+                    <span class="direct-chat-name float-left">{x.ButDanh}</span>
+                    <span class="direct-chat-timestamp float-right">{x.NgayViet}</span>
                 </div>
                 <img class="direct-chat-img" style={{height:"30px"}} src={x.Avatar == null ? "/Asset/Web/wp-content/themes/madara/images/no-avatar.png" : x.Avatar} alt="message user image" />
                 <div class="direct-chat-text">
@@ -43,16 +60,16 @@ class TraoDoi extends React.Component {
                 <div class="card-header ui-sortable-handle" style={{ cursor: "move" }}>
                     <h3>Trao đổi nhóm</h3>
                 </div>
-                <div class="card-body" style={{ display: "block", "overflow-y":"scroll", height:"500px"}}>
+                <div class="card-body" style={{ display: "block", "overflow-y": "scroll", height: "500px" }} id="phanChat">
                     <div class="direct-chat-messages">
                         {html}
                     </div>
                 </div>
                 <div class="card-footer" style={{ display: "block" }}>
                     <div class="input-group">
-                        <input type="text" name="message" placeholder="Type Message ..." class="form-control"/>
+                        <input type="text" name="message" placeholder="Type Message ..." class="form-control" id="message" onKeyUp={(e) => this.enterChat(e)} />
                         <span class="input-group-append">
-                            <button type="button" class="btn btn-primary" onClick={(e) => this.chat(e.tagert)}>Send</button>
+                            <button type="button" class="btn btn-primary" onClick={this.chat}>Send</button>
                             </span>
                             </div>
                     </div>
@@ -60,7 +77,7 @@ class TraoDoi extends React.Component {
             </div>)
     }
 }
-const readerTraoDoi = (data) => {
+const readerTraoDoi =  (data) => {
     ReactDOM.render(<TraoDoi data={data} />, document.getElementById('traoDoi'))
 }
 $(document).ready(async function () {
@@ -69,6 +86,12 @@ $(document).ready(async function () {
     dataChat = [...data]
     readerTraoDoi(dataChat);
     chat = $.connection.traoDoi_Nhom;
-    chat.server.message.joinRoom(maNhom + "")
-    
+    chat.client.message = function (data) {
+        debugger
+        dataChat.push(data)
+        readerTraoDoi(dataChat)
+    }
+    $.connection.hub.start().done(function () {
+        chat.server.joinRoom(maNhom)
+    })
 })
